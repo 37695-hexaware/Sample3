8bf1e07a59aec502756998337c2fd77de7c71efc
37695-hexaware <37695.hexaware@gmail.com>
2017-07-06 12:14:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+var Alexa = require("alexa-sdk");
+
+var express = require("express");
+var https = require('https');
+var app = express();
+
+var port = process.env.PORT || 8081;
+
+//app.use(express.logger());
+
+app.get('/', function(request, response) {
+  console.log('[support dash] processing get request');
+  response.send('Hello World 2!');
+});
+
+
+
+console.log("eg");
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+    console.log("eg1");
+
+};
+
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('SayHello');
+    },
+    'HelloWorldIntent': function () {
+        this.emit('SayHello')
+    },
+    'SayHello': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
+
+console.log(' port ' + port);
+
+https.createServer(function (req, res) {
+    handler(req);
+}).listen(port);
